ffb2bcc5b625be0ec5d768dbb3afe73a0fb11492
AverySuen119 <r.sun.17@student.scu.edu.au>
2025-10-16 12:32:00 +0800

    admin-site

--- admin-site/src/app/app.component.ts
+++ admin-site/src/app/app.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+// app.module.ts
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  standalone: false,
-  styleUrl: './app.component.css'
+import { AppComponent } from './app.component';
+import { AdminDashboardComponent } from './components/dashboard.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/events', pathMatch: 'full' },
+  { 
+    path: '', 
+    component: AdminDashboardComponent,
+    children: [
+
+    ]
+  },
+  { path: '**', redirectTo: '/events' }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminDashboardComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppComponent {
-  title = 'admin-site';
-}
+export class AppModule { }
--- admin-site/src/app/app.module.ts
+++ admin-site/src/app/app.module.ts
@@ -1,20 +1,40 @@
+// app.module.ts
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/events', pathMatch: 'full' },
+  { 
+    path: '', 
+    component: DashboardComponent,
+    children: [
+    
+    ]
+  },
+  { path: '**', redirectTo: '/events' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [
-    provideClientHydration(withEventReplay())
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
