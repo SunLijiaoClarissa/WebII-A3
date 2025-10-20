c7f87e7dfa3dce92ee2834b4de6fa7c565576de9
SunLijiaoClarissa <181180136+SunLijiaoClarissa@users.noreply.github.com>
2025-10-20 11:46:04 +0800

    update files for deploy

--- server-API/db-details.js
+++ server-API/db-details.js
@@ -2,7 +2,7 @@ module.exports =
 {
     host: "localhost",
     port: 3306,
-    user: "rsun17_rsun17",
-    password: "61@w@#xOSVYO",
-    database: "rsun17_charityevents_db"
+    user: "lsun32_lsun32",
+    password: "UtJ{%3lmO_NB",
+    database: "lsun32_charityevents_db"
 };
